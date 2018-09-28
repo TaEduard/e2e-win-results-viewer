01022a90bde4109b14deb7a22c9e3867f177614d
TaEduard <etamsa@cloudbasesolutions.com>
2018-09-28 15:32:27 +0300

    Refactor landingpage.py

--- landingpage.py
+++ landingpage.py
@@ -1,65 +1,28 @@
-from constants import *
 import os
+from constants import *
 
-def add_job_name(job):
-    job_res_path = job["jobResultsDir"]
-    job_name = job['jobName']
-    output = "<span style=' margin-left: 10px;' ><a href=%s>%s</a></span>" % (job_res_path, job_name)
+
+def add_matrix_to_dict(path, name, dict):
+    if name not in dict:
+        dict[name] = path
+    return dict
+
+def add_job(name, matrix_dict, config):
+    output = "<span style=' margin-left: 10px;' ><a>%s</a></span>" % name
+    output += "<span style=' margin-left: 10px;'><a href=%s>MATRIX</a></span>" % os.path.relpath(
+            matrix_dict[name], config["outputFolder"])
     return output
 
-def add_job_summary_matrix(job, job_type):
-    job_summary_matrix_path = os.path.join(job_type, job["jobName"], "summary_matrix")
-    job_summary_matrix_name = JOB_SUMMARY_MATRIX_NAME % {"job_name": job['jobName']}
-    output = "<span style=' margin-left: 10px;'><a href=%s>SUMMARY</a></span>" % (os.path.join(job_summary_matrix_path, job_summary_matrix_name))
-    return output
-
-def add_job_test_matrix(job, job_type):
-    job_summary_matrix_path = os.path.join(job_type, job["jobName"], "test_matrix")
-    job_summary_matrix_name = JOB_TEST_MATRIX_NAME % {"job_name": job['jobName']}
-    output = "<span style=' margin-left: 10px;'><a href=%s>TEST MATRIX</a></span>" % (os.path.join(job_summary_matrix_path, job_summary_matrix_name))
-    return output
-
-
-def add_custom_acs_job(job):
+def add_jobs(matrix_dict, config):
     output = "<li><ul>"
-    job_test_results_path = os.path.join("custom_acs", job["jobName"], "test_matrix")
-    for pr in os.listdir(os.path.join(results, job_test_results_path)):
-        pr_matrix_path = os.path.join(job_test_results_path, pr , "%s-%s-test-matrix.html" % (job["jobName"], pr))
-
-        if not os.path.isdir(os.path.join(results, job_test_results_path, pr)):
-            continue
-        output += "<li><span style=' margin-left: 10px;'><a href=%s>%s : TEST MATRIX</a></span>" % (pr_matrix_path, pr)
-        output += "</li>"
+    for name in matrix_dict:
+        output += "<li>%s</li>" % add_job(name, matrix_dict, config)
     output += "</ul></li>"
     return output
 
-def add_job(job, job_type):
-    output = add_job_name(job)
-    if job_type == "custom_acs":
-        # jobs are per pr
-        output += add_job_summary_matrix(job, job_type)
-        output += add_job_test_matrix(job, job_type)
-        output += add_custom_acs_job(job)
-        return output
-    output += add_job_test_matrix(job, job_type)
-    output += add_job_summary_matrix(job, job_type)
-    return output
-
-def add_jobs(job_type, jobs):
-    output = "<li>%s</li>" % job_type
-    output += "<li><ul>"
-    for job in jobs:
-        output += "<li>%s</li>" % add_job(job, job_type)
-    output += "</ul></li>"
-    return output
-        
-def create_landing_page(jobs, results_folder):
-    global results 
-    results = results_folder
+def create_landing_page(matrix_dict, config):
     output = "<html><body>Test reports<ul>"
-    for job_type in jobs.keys():
-        if len(jobs[job_type]) > 0:
-            output += add_jobs(job_type, jobs[job_type])
+    output += add_jobs(matrix_dict,config)
     output += "</ul></body></html>"
-
     return output
+
